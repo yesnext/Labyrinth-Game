b8d765261ccc3bb749a5c58e76aa75dfc1e6da6b
Ahmed Fathy <76529434+yesnext@users.noreply.github.com>
2024-12-12 18:12:48 +0200

    Revert "it fucking works ya walad"

--- Assets/scripts/WrongTileKill.cs
+++ Assets/scripts/WrongTileKill.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class WrongTileKill : MonoBehaviour
 {
     public GameObject beginPoint;
-    
     // Start is called before the first frame update
     void Start()
     {
