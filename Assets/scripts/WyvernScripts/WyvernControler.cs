7bdc88a802957fce5eae75c6ef5af6d34da94da1
Youssef Hesham <youssefhesham104@gmail.com>
2024-12-13 00:27:02 +0200

    wyvern Done

--- Assets/scripts/WyvernScripts/WyvernControler.cs
+++ Assets/scripts/WyvernScripts/WyvernControler.cs
@@ -1,73 +1,94 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 public class WyvernControler : UniversalEnemyNeeds
 {
-    public PatrolPoints[] PatrolPoints; 
-    public float speed = 3.0f; 
-    public float attackRange = 10.0f; 
+    public PatrolPoints[] PatrolPoints;
+    public float speed = 3.0f;
+    public float attackRange = 10.0f;
     public float attackCooldown = 2.0f;
-    public float attackDuration = 1.0f; 
-
+    public float attackDuration = 1.0f;
+    private ProjectilePoint projectilePoint;
+    public EnemyProjectile projectile;
     private int CurrentPatrolPoint = 0;
     private float lastAttackTime = 0.0f;
     private bool isAttacking = false;
     private float Patroldistance;
     private Vector2 PatrolDirection;
+    private int numberofpatrolpoints;
+    private int nextpatrolpoint;
     void Start()
     {
         player = FindObjectOfType<PlayerStats>();
         PatrolPoints = FindObjectsOfType<PatrolPoints>();
+        numberofpatrolpoints = PatrolPoints.Length;
+        projectilePoint = FindObjectOfType<ProjectilePoint>();
     }
 
     // Update is called once per frame
-    
+
     void Update()
     {
-        Followplayer();
-        GhangedirectionFollow();
+        if (Time.time - lastAttackTime > attackCooldown)
+        {
+            Followplayer();
+            GhangedirectionFollow();
+            Attack();
+        }
+        else
+        {
+            Patrol();
+            GhangedirectionPatrol();
 
+        }
     }
     public void FixedUpdate()
     {
         direction = (player.transform.position - transform.position).normalized;
         distance = Vector2.Distance(transform.position, player.transform.position);
-        Patroldistance = Vector2.Distance(transform.position,PatrolPoints[CurrentPatrolPoint].transform.position);
+        Patroldistance = Vector2.Distance(transform.position, PatrolPoints[CurrentPatrolPoint].transform.position);
         PatrolDirection = (PatrolPoints[CurrentPatrolPoint].transform.position - transform.position).normalized;
     }
     void Patrol()
     {
-        // Move towards the current waypoint
-        Transform targetWaypoint = PatrolPoints[CurrentPatrolPoint].transform;
-        Vector3 direction = (targetWaypoint.position - transform.position).normalized;
-        transform.position += direction * speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, PatrolPoints[CurrentPatrolPoint].transform.position, EnemySpeed * Time.deltaTime);
 
         // Check if we reached the waypoint
-        if (Vector3.Distance(transform.position, targetWaypoint.position) < 0.1f)
+        if (Patroldistance < 0.1f)
         {
-            // Move to the next waypoint
-            CurrentPatrolPoint = (CurrentPatrolPoint + 1) % PatrolPoints.Length;
+            while (nextpatrolpoint == CurrentPatrolPoint)
+                nextpatrolpoint = Random.Range(0, numberofpatrolpoints);
+            CurrentPatrolPoint = nextpatrolpoint;
+        }
+    }
+    public void GhangedirectionPatrol()
+    {
+        if (PatrolDirection.x > 0 && !isFacingRight)
+        {
+
+            scale = transform.localScale;
+            scale.x *= -1;
+            isFacingRight = true;
+            transform.localScale = scale;
+
+        }
+        else if (PatrolDirection.x < 0 && isFacingRight)
+        {
+            scale = transform.localScale;
+            scale.x *= -1;
+            isFacingRight = false;
+            transform.localScale = scale;
         }
     }
 
-    void AttackPlayer()
+    void Attack()
     {
-        // Move towards the player
-        Vector3 direction = (player.transform.position - transform.position).normalized;
-        transform.position += direction * speed * Time.deltaTime;
-
-        // Check if close enough to attack
-        if (Vector3.Distance(transform.position, player.transform.position) < 1.0f)
+        if (distance < 6.0f)
         {
-            // Perform attack (you can replace this with your attack logic)
-            Debug.Log("Wyvern attacks the player!");
-
-            // Set the last attack time
+            Instantiate(projectile, projectile.transform.position, projectilePoint.transform.rotation);
             lastAttackTime = Time.time;
-
-            // Start the attack animation (if any)
-            StartCoroutine(PerformAttack());
         }
     }
 
