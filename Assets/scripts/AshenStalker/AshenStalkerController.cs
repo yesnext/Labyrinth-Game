81cbef2cf8d2fe0fd11cd642e024679fdb4b511a
yesnext <ah625088@gmail.com>
2024-12-16 00:55:45 +0200

    Revert "Merge pull request #64 from yesnext/animcodehopefully"
    This reverts commit 0cad0a0f5360196f7f91f441713ab2f2ec068419, reversing
    changes made to 1ced595347c13046cc9bac96468a4651f8b4f42e.

--- Assets/scripts/AshenStalker/AshenStalkerController.cs
+++ Assets/scripts/AshenStalker/AshenStalkerController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-
 public class AshenStalkerController : UniversalEnemyNeeds
 {
     public float Summonscooldown = 100;
@@ -17,7 +16,6 @@ public class AshenStalkerController : UniversalEnemyNeeds
     protected float LastRangAttackTime;
     public float RangAttackCooldown = 2.0f;
     public float RangeAttackDistance = 5;
-    public float CurrentSpeed;
     public SpriteRenderer Disappear;
     public float appeardistance = 1;
     public GameObject minions;
@@ -25,14 +23,11 @@ public class AshenStalkerController : UniversalEnemyNeeds
     private SummonsSpawnLocation[] SpawnLocation;
     private Transform ProjectilePoint;
     private Rigidbody2D ShadoStep;
-    private Animator anim;
-    private BossesDefeated bs;
 
     // Start is called before the first frame update
     void Start()
     {
-        bs = FindObjectOfType<BossesDefeated>();
-        anim = GetComponent<Animator>();
+
         player = FindObjectOfType<PlayerStats>();
         Disappear = GetComponent<SpriteRenderer>();
         ShadoStep = GetComponent<Rigidbody2D>();
@@ -41,14 +36,11 @@ public class AshenStalkerController : UniversalEnemyNeeds
         if(player.GetComponent<BossesDefeated>().AshenStalker){
             Destroy(this.gameObject);
         }
-        OriginalSpeed = EnemySpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CurrentSpeed = ShadoStep.velocity.magnitude;
-        anim.SetFloat("Speed",CurrentSpeed);
         if (aggro)
         {
             ChangedDirectionFollow();
@@ -125,19 +117,6 @@ public class AshenStalkerController : UniversalEnemyNeeds
     {
         MeleeAttacking = true;
         Disappear.enabled = true;
-        //anim
-        int rnd = Random.Range(0, 3);
-        switch(rnd){
-            case 0:
-                anim.SetInteger("attackSelect",rnd);
-                break;
-            case 1:
-                anim.SetInteger("attackSelect",rnd);
-                break;
-            case 2:
-                anim.SetInteger("attackSelect",rnd);
-                break;
-        }
         yield return new WaitForSeconds(MeleeAttackAnimationDuration);
         EnemySpeed = OriginalSpeed;
         IsLunging = false;
@@ -147,15 +126,12 @@ public class AshenStalkerController : UniversalEnemyNeeds
     public IEnumerator Shoot()
     {
         RangAttacking = true;
-        //anim
-        anim.SetBool("isShoot",RangAttacking);
         yield return new WaitForSeconds(RangAttackAnimationDuration);
         GameObject projectile = Instantiate(Projectile, ProjectilePoint.position, ProjectilePoint.rotation);
         EnemyProjectile projectileController = projectile.GetComponent<EnemyProjectile>();
         projectileController.Intialize(RangeAttackDamage, RangeAttackSpeed);
         LastRangAttackTime = Time.time;
         RangAttacking = false;
-        anim.SetBool("isShoot",RangAttacking);
     }
     public override void TakeDamage(int damage)
     {
@@ -165,10 +141,6 @@ public class AshenStalkerController : UniversalEnemyNeeds
             if (Health <= 0)
             {
                 player.GetComponent<BossesDefeated>().AshenStalker = true;
-                if(bs.AshenStalker){
-                    anim.SetBool("IsDead",bs.AshenStalker);
-
-                }
                 Destroy(this.gameObject);
             }
         }
